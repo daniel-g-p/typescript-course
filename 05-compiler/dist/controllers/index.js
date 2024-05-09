39cdfdb9cf2bdc3f3ca7fa80770896bcf976d3f0
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2024-05-09 17:21:20 +0200

    Study compiler options

--- 05-compiler/dist/controllers/index.js
+++ 05-compiler/dist/controllers/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res, next) => {
+exports.default = (function (req, res, next) {
     return res.send("Hello World");
-};
+});
+//# sourceMappingURL=index.js.map
--- 05-compiler/dist/index.js
+++ 05-compiler/dist/index.js
@@ -3,9 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const index_js_1 = __importDefault(require("./router/index.js"));
-const app = (0, express_1.default)();
+var express_1 = __importDefault(require("express"));
+var index_js_1 = __importDefault(require("./router/index.js"));
+var app = (0, express_1.default)();
 app.use("/", index_js_1.default);
 app.listen(3000);
 console.log("Server running on http://localhost:3000");
+//# sourceMappingURL=index.js.map
